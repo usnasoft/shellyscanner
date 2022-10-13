674b352a0accb7594ca8a17f28bc72254dc7465e
usnasoft <38289708+usnasoft@users.noreply.github.com>
2022-10-13 18:47:10 +0200

    Update rundebug.cmd

--- rundebug.cmd
+++ rundebug.cmd
@@ -1,1 +1,1 @@
-java -D"org.slf4j.simpleLogger.log.it.usna=debug" -D"org.slf4j.simpleLogger.showDateTime=true" -jar C:\Users\201267\Personale\workspace\shellyscan\target\shellyscan-0.5.5-SNAPSHOT.jar -fullscan
+java -D"org.slf4j.simpleLogger.log.it.usna=trace" -D"org.slf4j.simpleLogger.showDateTime=true" -jar C:\Users\201267\Personale\workspace\shellyscan\target\shellyscan-0.x.x-SNAPSHOT.jar -fullscan
