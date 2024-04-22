6d96009fd2a55f2128955b7c14abda3391e68cfb
201267 <a.flaccomio@sispi.it>
2024-04-22 15:14:07 +0200

    exchange

--- src/main/java/it/usna/shellyscan/view/UptimeCellRenderer.java
+++ src/main/java/it/usna/shellyscan/view/UptimeCellRenderer.java
@@ -17,6 +17,27 @@ public class UptimeCellRenderer extends DefaultTableCellRenderer {
 	public void setMode(String mode) {
 		uptimeMode = UptimeMode.valueOf(mode);
 	}
+
+//	@Override
+//	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+//		Component l = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+//		if(value != null) {
+//			if(uptimeMode == UptimeMode.DAY) {
+//				int s = ((Integer) value).intValue();
+//				final int gg = (int)(s / (3600 * 24));
+//				s = s % (3600 * 24);
+//				int hh = (int)(s / 3600);
+//				s = s % 3600;
+//				int mm = (int)(s / 60);
+//				s = s % 60;
+//				((JLabel)l).setText(String.format(LABELS.getString("col_uptime_as_day"), gg, hh, mm, s));
+//			} else if(uptimeMode == UptimeMode.FROM) {
+//				LocalDateTime since = LocalDateTime.now().minusSeconds(((Integer) value).intValue());
+//				((JLabel)l).setText(String.format(LABELS.getString("col_uptime_as_From"), since));
+//			} //else int value
+//		}
+//		return l;
+//	}
 	
 	@Override
 	public void setValue(Object value) {
