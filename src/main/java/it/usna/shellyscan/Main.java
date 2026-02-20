e5b8e2223ff264e4f5c7c9393ebf8d9a6cf063a1
201267 <a.flaccomio@sispi.it>
2026-02-20 11:20:59 +0100

    pom update

--- src/main/java/it/usna/shellyscan/Main.java
+++ src/main/java/it/usna/shellyscan/Main.java
@@ -35,7 +35,7 @@ public class Main {
 		System.setProperty("apple.eawt.quitStrategy", "CLOSE_ALL_WINDOWS"); // macOS specific - cmd-Q / -Dapple.eawt.quitStrategy=CLOSE_ALL_WINDOWS
 	}
 	public static final String APP_NAME = "Shelly Scanner";
-	public static final String VERSION = "1.3.3 alpha";
+	public static final String VERSION = "1.3.3 alpha0";
 	public static final String VERSION_CODE = "001.003.003r000"; // r0xx alpha; r1xx beta; r2xx stable
 	public static final Image ICON = Toolkit.getDefaultToolkit().createImage(Main.class.getResource("/images/ShSc24.png"));
 	public static final String BACKUP_FILE_EXT = "sbk";
